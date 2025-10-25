433c21bc11290fab2f03209daa36d3c207148964
hedilarbi <hedi.larbi@esprit.tn>
2025-10-25 21:44:58 +0100

    fdsw

--- app/(client)/sign-up-success/page.jsx
+++ app/(client)/sign-up-success/page.jsx
@@ -1,11 +1,3 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
